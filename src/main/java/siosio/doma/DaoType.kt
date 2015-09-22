db98fed9437858904788efad69fc5c28e7e13dfe
siosio <siosio12@gmail.com>
2015-09-22 20:41:52 +0900

    DaoTypeをリファクタリング

--- src/main/java/siosio/doma/DaoType.kt
+++ src/main/java/siosio/doma/DaoType.kt
@@ -5,26 +5,12 @@ import siosio.doma.inspection.dao.*
 import siosio.doma.psi.*
 
 
-public enum class DaoType(val annotationName: String) {
+public enum class DaoType(val annotationName: String, val rule: DaoInspectionRule, val extension: String = "sql") {
 
-  SELECT("org.seasar.doma.Select") {
-    override fun rule(): DaoInspectionRule = selectMethodRule
-  },
-  UPDATE("org.seasar.doma.Update") {
-    override fun rule(): DaoInspectionRule = updateMethodRule
-  },
-  INSERT("org.seasar.doma.Insert") {
-    override fun rule(): DaoInspectionRule = insertMethodRule
-  },
-  DELETE("org.seasar.doma.Delete") {
-    override fun rule(): DaoInspectionRule = deleteMethodRule
-  },
-  BATCH_INSERT("org.seasar.doma.BatchInsert") {
-    override fun rule(): DaoInspectionRule = batchInsertMethodRule
-  },
-  SCRIPT("org.seasar.doma.Script") {
-    override fun rule(): DaoInspectionRule = scriptMethodRule
-  };
-
-  abstract fun rule(): DaoInspectionRule
+  SELECT("org.seasar.doma.Select", selectMethodRule),
+  UPDATE("org.seasar.doma.Update", updateMethodRule),
+  INSERT("org.seasar.doma.Insert", insertMethodRule),
+  DELETE("org.seasar.doma.Delete", deleteMethodRule),
+  BATCH_INSERT("org.seasar.doma.BatchInsert", batchInsertMethodRule),
+  SCRIPT("org.seasar.doma.Script", scriptMethodRule, "script")
 }
