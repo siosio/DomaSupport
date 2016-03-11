2917bf05dbca6ac409d0de13d6ddbd2f7a6c3329
siosio <siosio12@gmail.com>
2016-03-12 01:21:33 +0900

    SQLファイル内のバインド変数コメントで一部の補完をサポート
    以下は、未対応
    * どっと(.)を使用したフィールドやメソッドの補完
    * リネーム

---
+++ src/main/java/siosio/doma/DomaUtils.kt
@@ -0,0 +1,51 @@
+package siosio.doma
+
+import com.intellij.openapi.module.*
+import com.intellij.openapi.project.*
+import com.intellij.openapi.roots.*
+import com.intellij.psi.*
+
+
+fun toDaoClass(sqlFile: PsiFile): PsiClass? {
+  val parent = sqlFile.parent
+  if (parent?.let { !it.isPhysical } ?: false) {
+    return null
+  }
+
+  val className = toDaoClassName(parent, "")
+  val project = sqlFile.project
+  val module = getModule(project, sqlFile)
+  return JavaPsiFacade.getInstance(project)
+      .findClasses(className, module.moduleScope).first()
+}
+
+fun toMethodName(sqlFile: PsiFile): String {
+  val name = sqlFile.name
+  val lastIndexOf = name.lastIndexOf('.')
+  return if (lastIndexOf == -1) {
+    name
+  } else {
+    name.substring(0, lastIndexOf)
+  }
+}
+
+private fun getModule(project: Project, element: PsiElement): Module {
+  val module = ProjectRootManager.getInstance(project)
+      .fileIndex
+      .getModuleForFile(element.containingFile.virtualFile)!!
+  return module
+}
+
+private fun toDaoClassName(dir: PsiDirectory?, className: String): String {
+  if (dir == null || dir.name == "META-INF") {
+    return className
+  }
+  return toDaoClassName(dir.parent,
+      dir.name +
+          if (className.isEmpty()) {
+            className
+          } else {
+            "." + className
+          }
+  )
+}
---
+++ src/main/java/siosio/doma/sql/ParameterCompletionContributor.kt
@@ -0,0 +1,38 @@
+package siosio.doma.sql
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.*
+import com.intellij.patterns.*
+import com.intellij.psi.*
+import com.intellij.util.*
+import siosio.doma.*
+
+open class ParameterCompletionContributor : CompletionContributor() {
+
+  init {
+    extend(CompletionType.BASIC,
+        PlatformPatterns.psiElement(PsiComment::class.java)
+            .inFile(PlatformPatterns.psiFile().withName(StandardPatterns.string().endsWith(".sql"))),
+        SqlParameterCompletionProvider())
+  }
+
+  private class SqlParameterCompletionProvider : CompletionProvider<CompletionParameters>() {
+    override fun addCompletions(parameters: CompletionParameters, p1: ProcessingContext?, result: CompletionResultSet) {
+      val originalFile = parameters.originalFile
+      parameters.originalPosition?.text?.let {
+        val text = it.replace("/*", "").replace("*/", "")
+        if (!text.trim().isEmpty() && originalFile.text.get(parameters.offset - 1).isWhitespace()) {
+          result.stopHere()
+          return
+        }
+      }
+
+      toDaoClass(originalFile)?.let {
+        it.findMethodsByName(toMethodName(originalFile), false).first()?.parameterList?.parameters?.forEach {
+          result.addElement(VariableLookupItem(it))
+        }
+      }
+      result.stopHere()
+    }
+  }
+}
