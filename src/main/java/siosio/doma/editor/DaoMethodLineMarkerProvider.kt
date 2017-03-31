b849f97445220a560d75261b6777055def9149b2
siosio <siosio12@gmail.com>
2017-03-31 10:55:00 +0900

    SQLファイルのアイコンを最初のアノテーションの行に表示するように対応

--- src/main/java/siosio/doma/editor/DaoMethodLineMarkerProvider.kt
+++ src/main/java/siosio/doma/editor/DaoMethodLineMarkerProvider.kt
@@ -4,6 +4,7 @@ import com.intellij.codeInsight.daemon.*
 import com.intellij.codeInsight.navigation.*
 import com.intellij.openapi.util.*
 import com.intellij.psi.*
+import com.intellij.psi.util.*
 import siosio.doma.*
 import siosio.doma.extension.*
 import siosio.doma.psi.*
@@ -15,30 +16,33 @@ import siosio.doma.psi.*
  */
 class DaoMethodLineMarkerProvider : RelatedItemLineMarkerProvider() {
 
-  override fun collectNavigationMarkers(
-      elements: List<PsiElement>,
-      result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>,
-      forNavigation: Boolean) {
-    super.collectNavigationMarkers(elements, result, forNavigation)
-  }
-
-  override fun collectNavigationMarkers(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>?) {
-    if (element !is PsiMethod || result == null) {
-      return
+    override fun collectNavigationMarkers(
+        elements: List<PsiElement>,
+        result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>,
+        forNavigation: Boolean) {
+        super.collectNavigationMarkers(elements, result, forNavigation)
     }
 
-    DaoType.valueOf(element)?.let {
-      PsiDaoMethod(element, it).findSqlFile()
-    }?.let {
-      val psiFile = element.project.findFile(it)
-      val builder = NavigationGutterIconBuilder.create(SQL_FILE_ICON)
-          .setTargets(psiFile).setTooltipText(DomaBundle.message("editor.goto-sql-file"))
-      result.add(builder.createLineMarkerInfo(element))
-    }
-  }
+    override fun collectNavigationMarkers(element: PsiElement,
+                                          result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>?) {
+        if (element !is PsiMethod || result == null) {
+            return
+        }
 
-  companion object {
-    private val SQL_FILE_ICON = IconLoader.getIcon("/icons/sql.png")
-  }
+        DaoType.valueOf(element)?.let {
+            PsiDaoMethod(element, it).findSqlFile()
+        }?.let {
+            val psiFile = element.project.findFile(it)
+            PsiTreeUtil.findChildrenOfType(element, PsiAnnotation::class.java).firstOrNull()?.let {
+                val builder = NavigationGutterIconBuilder.create(SQL_FILE_ICON)
+                    .setTargets(psiFile).setTooltipText(DomaBundle.message("editor.goto-sql-file"))
+                result.add(builder.createLineMarkerInfo(it))
+            }
+        }
+    }
+
+    companion object {
+        private val SQL_FILE_ICON = IconLoader.getIcon("/icons/sql.png")
+    }
 }
 
