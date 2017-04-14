b43808ad5ade89a8a2a763ea9a3c337335ae1940
siosio <siosio12@gmail.com>
2017-04-14 16:31:10 +0900

    インデント修正

--- src/main/java/siosio/doma/DomaUtils.kt
+++ src/main/java/siosio/doma/DomaUtils.kt
@@ -2,51 +2,48 @@ package siosio.doma
 
 import com.intellij.openapi.module.*
 import com.intellij.openapi.project.*
-import com.intellij.openapi.roots.*
 import com.intellij.psi.*
+import siosio.doma.extension.*
 
 fun toSqlFilePath(path: String) = "META-INF/$path"
 
 fun toDaoClass(sqlFile: PsiFile): PsiClass? {
-  val parent = sqlFile.parent
-  if (parent?.let { !it.isPhysical } ?: false) {
-    return null
-  }
+    val parent = sqlFile.parent
+    if (parent?.let { !it.isPhysical } ?: false) {
+        return null
+    }
 
-  val className = toDaoClassName(parent, "")
-  val project = sqlFile.project
-  val module = getModule(project, sqlFile)
-  return JavaPsiFacade.getInstance(project)
-      .findClasses(className, module.moduleScope).first()
+    val className = toDaoClassName(parent, "")
+    val project = sqlFile.project
+    val module = getModule(project, sqlFile)
+    return JavaPsiFacade.getInstance(project)
+        .findClasses(className, module.moduleScope).first()
 }
 
 fun toMethodName(sqlFile: PsiFile): String {
-  val name = sqlFile.name
-  val lastIndexOf = name.lastIndexOf('.')
-  return if (lastIndexOf == -1) {
-    name
-  } else {
-    name.substring(0, lastIndexOf)
-  }
+    val name = sqlFile.name
+    val lastIndexOf = name.lastIndexOf('.')
+    return if (lastIndexOf == -1) {
+        name
+    } else {
+        name.substring(0, lastIndexOf)
+    }
 }
 
 fun getModule(project: Project, element: PsiElement): Module {
-  val module = ProjectRootManager.getInstance(project)
-      .fileIndex
-      .getModuleForFile(element.containingFile.virtualFile)!!
-  return module
+    return project.findModule(element.containingFile.virtualFile)!!
 }
 
 private fun toDaoClassName(dir: PsiDirectory?, className: String): String {
-  if (dir == null || dir.name == "META-INF") {
-    return className
-  }
-  return toDaoClassName(dir.parent,
-      dir.name +
-          if (className.isEmpty()) {
+    if (dir == null || dir.name == "META-INF") {
+        return className
+    }
+    return toDaoClassName(dir.parent,
+        dir.name +
+        if (className.isEmpty()) {
             className
-          } else {
+        } else {
             "." + className
-          }
-  )
+        }
+    )
 }
