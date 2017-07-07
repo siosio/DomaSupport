2cacb0296415b22ab2eb368553d90e4bb8745a24
siosio <siosio12@gmail.com>
2017-07-07 15:04:27 +0900

    コメント内の補完の精度を向上

--- src/main/java/siosio/doma/sql/ParameterCompletionContributor.kt
+++ src/main/java/siosio/doma/sql/ParameterCompletionContributor.kt
@@ -11,34 +11,34 @@ open class ParameterCompletionContributor : CompletionContributor() {
 
     init {
         extend(CompletionType.BASIC,
-            PlatformPatterns.psiElement(PsiComment::class.java)
-                .inFile(PlatformPatterns.psiFile().withName(StandardPatterns.string().endsWith(".sql"))),
-            SqlParameterCompletionProvider())
+                PlatformPatterns.psiElement(PsiComment::class.java)
+                        .inFile(PlatformPatterns.psiFile().withName(StandardPatterns.string().endsWith(".sql"))),
+                SqlParameterCompletionProvider())
     }
 
     private class SqlParameterCompletionProvider : CompletionProvider<CompletionParameters>() {
 
         private val buildInFunctionList = listOf(
-            "escape",
-            "prefix",
-            "infix",
-            "suffix",
-            "roundDownTimePart",
-            "roundDownTimePart",
-            "roundDownTimePart",
-            "roundUpTimePart",
-            "roundUpTimePart",
-            "roundUpTimePart",
-            "isEmpty",
-            "isNotEmpty",
-            "isBlank",
-            "isNotBlank"
+                "escape",
+                "prefix",
+                "infix",
+                "suffix",
+                "roundDownTimePart",
+                "roundDownTimePart",
+                "roundDownTimePart",
+                "roundUpTimePart",
+                "roundUpTimePart",
+                "roundUpTimePart",
+                "isEmpty",
+                "isNotEmpty",
+                "isBlank",
+                "isNotBlank"
         )
 
         private val buildInFunctionLookupList = run {
             buildInFunctionList.map {
                 LookupElementBuilder.create(it)
-                    .withAutoCompletionPolicy(AutoCompletionPolicy.ALWAYS_AUTOCOMPLETE)
+                        .withAutoCompletionPolicy(AutoCompletionPolicy.ALWAYS_AUTOCOMPLETE)
             }
         }
 
@@ -48,25 +48,39 @@ open class ParameterCompletionContributor : CompletionContributor() {
             val originalFile = parameters.originalFile
             val text = parameters.originalPosition?.text?.let {
                 val text = it.substringAfter("/*%if")
-                    .substringAfter("/*")
-                    .substringBefore("*/")
-                    .trim()
+                        .substringAfter("/*")
+                        .substringBefore("*/")
+                        .trim()
                 text
             } ?: ""
 
             if (isBuiltInFunction(text)) {
                 result.addAllElements(buildInFunctionLookupList)
             } else {
-                toDaoClass(originalFile)
-                    ?.findMethodsByName(toMethodName(originalFile), false)
-                    ?.firstOrNull()
-                    ?.parameterList
-                    ?.parameters
-                    ?.map(::VariableLookupItem)
-                    ?.toList()
-                    ?.let { list ->
-                        result.addAllElements(list)
-                    }
+                val paramList = toDaoClass(originalFile)
+                        ?.findMethodsByName(toMethodName(originalFile), false)
+                        ?.firstOrNull()
+                        ?.parameterList
+                        ?.parameters
+                if (text.contains("(")) {
+                    val paramText = text.substringAfter("(")
+                    paramList
+                            ?.filter {
+                                it.name?.startsWith(paramText) ?: false
+                            }
+                            ?.map(::VariableLookupItem)
+                            ?.toList()
+                            ?.let { list ->
+                                result.addAllElements(list)
+                            }
+                } else {
+                    paramList
+                            ?.map(::VariableLookupItem)
+                            ?.toList()
+                            ?.let { list ->
+                                result.addAllElements(list)
+                            }
+                }
             }
             result.stopHere()
         }
