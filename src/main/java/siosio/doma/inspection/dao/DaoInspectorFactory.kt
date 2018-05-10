fd54a2058aab128eed5dc1cfc506331cc661a0e8
siosio <siosio12@gmail.com>
2018-05-11 07:44:32 +0900

    Insertのルールを別のファイルに切り出し

--- src/main/java/siosio/doma/inspection/dao/DaoInspectorFactory.kt
+++ src/main/java/siosio/doma/inspection/dao/DaoInspectorFactory.kt
@@ -1,8 +1,6 @@
 package siosio.doma.inspection.dao
 
-import com.intellij.psi.*
 import siosio.doma.extension.*
-import siosio.doma.inspection.dao.quickfix.*
 
 val parameterTypeCheck: ParameterRule.() -> Unit = {
     message = "inspection.dao.entity-param-not-found"
@@ -17,44 +15,6 @@ val parameterTypeCheck: ParameterRule.() -> Unit = {
         }
     }
 }
-
-val insertMethodRule =
-    rule {
-        sql(false)
-
-        // 引数チェック
-        parameterRule(parameterTypeCheck)
-
-        returnRule {
-            rule = block@ { daoMethod ->
-                val entityParameter =
-                    when {
-                        daoMethod.useSqlFile() -> daoMethod.parameterList.parameters.firstOrNull { it.isEntity() }
-                        else -> daoMethod.parameterList.parameters.firstOrNull()
-                    } ?: return@block true
-
-                if (entityParameter.isEntity() && entityParameter.isImmutableEntity()) {
-                    message = "inspection.dao.immutable-insert-return-type"
-                    quickFix = ImmutableEntityReturnTypeQuickFix()
-                    messageArgs = arrayOf(entityParameter.type.canonicalText)
-                    // Resultの型パラメータまでチェックする
-                    if (type.isAssignableFrom(PsiType.getTypeByName("org.seasar.doma.jdbc.Result", daoMethod.project, resolveScope))) {
-                        daoMethod.returnTypeElement?.innermostComponentReferenceElement?.typeParameters?.let { 
-                            it.firstOrNull()?.isAssignableFrom(entityParameter.type)
-                        } ?: false
-                    } else {
-                        false
-                    }
-                } else if (entityParameter.isEntity() && entityParameter.isImmutableEntity().not()) {
-                    message = "inspection.dao.mutable-insert-return-type"
-                    type.isAssignableFrom(PsiType.INT)
-                } else {
-                    // 引数がまともじゃない場合はとりあえずOKにする
-                    true
-                }
-            }
-        }
-    }
 
 val updateMethodRule =
     rule {
