6f12685dc59d7aacc57ba7b96ccd6f24d4de14d1
siosio <siosio12@gmail.com>
2020-09-04 22:46:25 +0900

    :lipstick: format code

--- src/main/java/siosio/doma/inspection/dao/SelectMethodRule.kt
+++ src/main/java/siosio/doma/inspection/dao/SelectMethodRule.kt
@@ -12,49 +12,49 @@ import kotlin.reflect.jvm.internal.impl.descriptors.TypeParameterUtilsKt
 import kotlin.reflect.jvm.internal.impl.types.typeUtil.TypeUtilsKt
 
 val selectMethodRule =
-        rule {
-            sql(true)
+    rule {
+        sql(true)
 
-            // SelectOptionsの検査
-            parameterRule {
-                message = "inspection.dao.multi-SelectOptions"
-                quickFix = { QuickFixFactory.getInstance().createDeleteFix(it, DomaBundle.message("quick-fix.remove")) }
-                rule = { filter { it.type.canonicalText == "org.seasar.doma.jdbc.SelectOptions" }.size in (0..1) }
-                errorElements = {
-                    it.parameterList.parameters.filter { it.type.canonicalText == "org.seasar.doma.jdbc.SelectOptions" }
-                }
+        // SelectOptionsの検査
+        parameterRule {
+            message = "inspection.dao.multi-SelectOptions"
+            quickFix = { QuickFixFactory.getInstance().createDeleteFix(it, DomaBundle.message("quick-fix.remove")) }
+            rule = { filter { it.type.canonicalText == "org.seasar.doma.jdbc.SelectOptions" }.size in (0..1) }
+            errorElements = {
+                it.parameterList.parameters.filter { it.type.canonicalText == "org.seasar.doma.jdbc.SelectOptions" }
             }
+        }
 
-            // strategyにSTREAMを指定した場合の検査
-            parameterRule {
-                message = "inspection.dao.function-strategy"
-                errorElement = { psiDaoMethod -> psiDaoMethod.daoAnnotation.originalElement }
-                rule = { psiDaoMethod ->
-                    psiDaoMethod.daoAnnotation.run {
-                        if (findAttributeValue("strategy")?.text?.contains("STREAM") == true) {
-                            filter { PsiTypesUtil.getPsiClass(it.type)?.qualifiedName == "java.util.function.Function" }.size == 1
-                        } else {
-                            true
-                        }
+        // strategyにSTREAMを指定した場合の検査
+        parameterRule {
+            message = "inspection.dao.function-strategy"
+            errorElement = { psiDaoMethod -> psiDaoMethod.daoAnnotation.originalElement }
+            rule = { psiDaoMethod ->
+                psiDaoMethod.daoAnnotation.run {
+                    if (findAttributeValue("strategy")?.text?.contains("STREAM") == true) {
+                        filter { PsiTypesUtil.getPsiClass(it.type)?.qualifiedName == "java.util.function.Function" }.size == 1
+                    } else {
+                        true
                     }
                 }
             }
         }
+    }
 
-val kotlinSelectMethodRule = 
-        kotlinRule { 
-            sql(true)
-            
-            // SelectOptionsの検査
-            parameterRule {
-                message = "inspection.dao.multi-SelectOptions"
-                quickFix = { QuickFixFactory.getInstance().createDeleteFix(it, DomaBundle.message("quick-fix.remove")) }
-                rule = { filterSelectOptions().size in (0..1) }
-                errorElements = {
-                    it.valueParameters.filterSelectOptions()
-                }
+val kotlinSelectMethodRule =
+    kotlinRule {
+        sql(true)
+
+        // SelectOptionsの検査
+        parameterRule {
+            message = "inspection.dao.multi-SelectOptions"
+            quickFix = { QuickFixFactory.getInstance().createDeleteFix(it, DomaBundle.message("quick-fix.remove")) }
+            rule = { filterSelectOptions().size in (0..1) }
+            errorElements = {
+                it.valueParameters.filterSelectOptions()
             }
         }
+    }
 
 private fun List<KtParameter>.filterSelectOptions(): List<KtParameter> {
     return filter {
